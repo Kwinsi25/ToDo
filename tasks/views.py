456e18db440dfac5443cf4ae23154264f7f72eff
Kwinsi Sadhu <kwinsi_sadhu@cybercom.in>
2024-07-23 10:58:32 +0530

    design change

--- tasks/views.py
+++ tasks/views.py
@@ -1,32 +1,32 @@
-# tasks/views.py
+
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib import messages
 from .models import Task
 from .forms import TaskForm
 from django.utils import timezone
-from datetime import datetime, timedelta
+from datetime import  timedelta
 
 def upcoming_tasks(request):
-    tomorrow = timezone.now().date() + timedelta(days=1)  # Tomorrow's date
+    tomorrow = timezone.now().date() + timedelta(days=1)  
     tomorrow_tasks = Task.objects.filter(reminder_time__date__gte=tomorrow, completed=False).order_by('reminder_time')
 
     upcoming_count = Task.objects.filter(reminder_time__date__gte=tomorrow, completed=False).count()
-    # Calculate today's tasks count
+    
     today = timezone.now().date()
     today_count = Task.objects.filter(reminder_time__date=today, completed=False).count()
 
-    # Filter personal tasks
+    
     personal_tasks = Task.objects.filter(task_type='personal', completed=False).order_by('reminder_time')
     personal_count = personal_tasks.count()
 
-     # Filter work tasks
+     
     work_tasks = Task.objects.filter(task_type='work', completed=False).order_by('reminder_time')
     work_count = work_tasks.count()
 
     return render(request, 'tasks/upcoming_tasks.html', {'tasks': tomorrow_tasks,'upcoming_count': upcoming_count,'today_count': today_count,'personal_count':personal_count,'work_count':work_count})
 
 def today_tasks(request):
-    today = timezone.now().date()  # Today's date
+    today = timezone.now().date()  
     today_tasks = Task.objects.filter(reminder_time__date=today, completed=False).order_by('reminder_time')
 
     today_count = Task.objects.filter(reminder_time__date=today, completed=False).count()
@@ -34,11 +34,11 @@ def today_tasks(request):
     tomorrow = timezone.now().date() + timedelta(days=1)
     upcoming_count = Task.objects.filter(reminder_time__date__gte=tomorrow, completed=False).count()
 
-    # Filter personal tasks
+    
     personal_tasks = Task.objects.filter(task_type='personal', completed=False).order_by('reminder_time')
     personal_count = personal_tasks.count()
 
-    # Filter work tasks
+    
     work_tasks = Task.objects.filter(task_type='work', completed=False).order_by('reminder_time')
     work_count = work_tasks.count()
     
@@ -46,17 +46,17 @@ def today_tasks(request):
 
 
 def personal_tasks(request):
-    tomorrow = timezone.now().date() + timedelta(days=1)  # Tomorrow's date
+    tomorrow = timezone.now().date() + timedelta(days=1)  
     upcoming_count = Task.objects.filter(reminder_time__date__gte=tomorrow, completed=False).count()
-    # Calculate today's tasks count
+    
     today = timezone.now().date()
     today_count = Task.objects.filter(reminder_time__date=today, completed=False).count()
 
-    # Filter personal tasks
+    
     personal_tasks = Task.objects.filter(task_type='personal', completed=False).order_by('reminder_time')
     personal_count = personal_tasks.count()
 
-    # Filter work tasks
+    
     work_tasks = Task.objects.filter(task_type='work', completed=False).order_by('reminder_time')
     work_count = work_tasks.count()
     
@@ -70,17 +70,17 @@ def personal_tasks(request):
     })
 
 def work_tasks(request):
-    tomorrow = timezone.now().date() + timedelta(days=1)  # Tomorrow's date
+    tomorrow = timezone.now().date() + timedelta(days=1)  
     upcoming_count = Task.objects.filter(reminder_time__date__gte=tomorrow, completed=False).count()
-    # Calculate today's tasks count
+    
     today = timezone.now().date()
     today_count = Task.objects.filter(reminder_time__date=today, completed=False).count()
 
-    # Filter personal tasks
+    
     personal_tasks = Task.objects.filter(task_type='personal', completed=False).order_by('reminder_time')
     personal_count = personal_tasks.count()
 
-    # Filter work tasks
+    
     work_tasks = Task.objects.filter(task_type='work', completed=False).order_by('reminder_time')
     work_count = work_tasks.count()
     
@@ -98,21 +98,21 @@ def task_list(request):
     tomorrow = timezone.now().date() + timedelta(days=1)
     upcoming_count = Task.objects.filter(reminder_time__date__gte=tomorrow, completed=False).count()
 
-    # Calculate today's tasks count
+    
     today = timezone.now().date()
     today_count = Task.objects.filter(reminder_time__date=today, completed=False).count()
 
-    # Filter personal tasks
+    
     personal_tasks = Task.objects.filter(task_type='personal', completed=False).order_by('reminder_time')
     personal_count = personal_tasks.count()
 
 
-    # Filter work tasks
+    
     work_tasks = Task.objects.filter(task_type='work', completed=False).order_by('reminder_time')
     work_count = work_tasks.count()
 
     if request.method == 'POST':
-        # Calculate upcoming tasks count (tomorrow and onward)
+        
         
         date_filter = request.POST.get('date', None)
         if date_filter:
@@ -120,9 +120,9 @@ def task_list(request):
                 date_filter = timezone.datetime.strptime(date_filter, '%Y-%m-%d').date()
                 tasks = Task.objects.filter(reminder_time__date=date_filter).order_by('reminder_time')
             except ValueError:
-                tasks = Task.objects.none()  # Handle invalid date format gracefully
+                tasks = Task.objects.none()  
         else:
-            tasks = Task.objects.none()  # Handle no date filter provided
+            tasks = Task.objects.none()  
     else:
         tasks = Task.objects.filter(reminder_time__isnull=False, completed=False).order_by('reminder_time')
     
@@ -136,11 +136,21 @@ def task_list(request):
 
 def add_task(request):
     tomorrow = timezone.now().date() + timedelta(days=1)
-    upcoming_count = Task.objects.filter(reminder_time__date__gte=tomorrow).count()
+    
+    upcoming_count = Task.objects.filter(reminder_time__date__gte=tomorrow, completed=False).count()
 
-    # Calculate today's tasks count
+    
     today = timezone.now().date()
-    today_count = Task.objects.filter(reminder_time__date=today).count()
+    
+    today_count = Task.objects.filter(reminder_time__date=today, completed=False).count()
+
+    
+    personal_tasks = Task.objects.filter(task_type='personal', completed=False).order_by('reminder_time')
+    personal_count = personal_tasks.count()
+
+    
+    work_tasks = Task.objects.filter(task_type='work', completed=False).order_by('reminder_time')
+    work_count = work_tasks.count()
     if request.method == 'POST':
         form = TaskForm(request.POST)
         if form.is_valid():
@@ -150,15 +160,25 @@ def add_task(request):
     else:
         form = TaskForm()
     return render(request, 'tasks/task_form.html', {'form': form,'upcoming_count': upcoming_count,
-        'today_count': today_count,})
+        'today_count': today_count,'personal_count':personal_count,
+        'work_count':work_count})
 
 def task_update(request, pk):
     tomorrow = timezone.now().date() + timedelta(days=1)
     upcoming_count = Task.objects.filter(reminder_time__date__gte=tomorrow).count()
 
-    # Calculate today's tasks count
+    
     today = timezone.now().date()
     today_count = Task.objects.filter(reminder_time__date=today).count()
+
+    
+    personal_tasks = Task.objects.filter(task_type='personal', completed=False).order_by('reminder_time')
+    personal_count = personal_tasks.count()
+
+    
+    work_tasks = Task.objects.filter(task_type='work', completed=False).order_by('reminder_time')
+    work_count = work_tasks.count()
+
     task = get_object_or_404(Task, pk=pk)
     if request.method == 'POST':
         form = TaskForm(request.POST, instance=task)
@@ -173,15 +193,25 @@ def task_update(request, pk):
     else:
         form = TaskForm(instance=task)
     return render(request, 'tasks/task_form.html', {'form': form,'upcoming_count': upcoming_count,
-        'today_count': today_count,})
+        'today_count': today_count,'personal_count':personal_count,
+        'work_count':work_count})
 
 def delete_task(request, pk):
     tomorrow = timezone.now().date() + timedelta(days=1)
     upcoming_count = Task.objects.filter(reminder_time__date__gte=tomorrow).count()
 
-    # Calculate today's tasks count
+    
     today = timezone.now().date()
     today_count = Task.objects.filter(reminder_time__date=today).count()
+
+    
+    personal_tasks = Task.objects.filter(task_type='personal', completed=False).order_by('reminder_time')
+    personal_count = personal_tasks.count()
+
+    
+    work_tasks = Task.objects.filter(task_type='work', completed=False).order_by('reminder_time')
+    work_count = work_tasks.count()
+
     task = get_object_or_404(Task, pk=pk)
     if request.method == 'POST':
         confirm = request.POST.get('confirm_delete', False)
@@ -194,4 +224,5 @@ def delete_task(request, pk):
             return redirect('task-list')
     
     return render(request, 'tasks/task_confirm_delete.html', {'task': task,'upcoming_count': upcoming_count,
-        'today_count': today_count,})
+        'today_count': today_count,'personal_count':personal_count,
+        'work_count':work_count})
